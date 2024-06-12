072aedba983b39b6d23b0b5e303ff5aee3e82c6c
DouglasIde <douglasymide@gmail.com>
2024-06-12 15:39:10 -0300

    Gerando os métodos GETTERS/SETTERS na classe Conta

--- src/conta/model/Conta.java
+++ src/conta/model/Conta.java
@@ -13,6 +13,46 @@ public class Conta {
 		this.titular = titular;
 		this.saldo = saldo;
 	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public int getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+
+	public int getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getTitular() {
+		return titular;
+	}
+
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+
+	public float getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(float saldo) {
+		this.saldo = saldo;
+	}
 	
 	
 }
