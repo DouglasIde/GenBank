d0cda9d69040f997091c777d7f0c82a7c45cb0db
DouglasIde <douglasymide@gmail.com>
2024-06-12 14:19:49 -0300

    Importando o Scanner

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,10 +1,13 @@
 package conta;
 
+import java.util.Scanner;
+
 public class Menu {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner leitura = new Scanner(System.in);
 	}
 
 }
