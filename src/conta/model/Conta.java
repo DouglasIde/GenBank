c6018ea8ea191b48d850462ff80f850cbf24d982
DouglasIde <douglasymide@gmail.com>
2024-06-12 15:37:37 -0300

    Criando um Construtor para a Classe Conta

--- src/conta/model/Conta.java
+++ src/conta/model/Conta.java
@@ -6,4 +6,13 @@ public class Conta {
 	private String titular;
 	private float saldo;
 	
+	public Conta(int numero, int agencia, int tipo, String titular, float saldo) {
+		this.numero = numero;
+		this.agencia = agencia;
+		this.tipo = tipo;
+		this.titular = titular;
+		this.saldo = saldo;
+	}
+	
+	
 }
